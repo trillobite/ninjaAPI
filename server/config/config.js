6643cb214cf0b3d2a88da23dab3ea6217919f9b1
patterncoder <patterncoder@gmail.com>
2015-11-30 10:04:05 -0800

    reset mongolab

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ module.exports = {
     production: {
         secret: 'ninjaApiRox',
         rootPath: rootPath,
-        db: 'mongodb://patterncoder:y5EQJ5m7C3@ds042688.mongolab.com:42688/ninja-api',
+        db: 'mongodb://patterncoder:y5EQJ5m7C3@ds054298.mongolab.com:54298/ninjaapi',
         port: process.env.PORT || 80,
         email_un: 'banquet.ninja@gmail.com',
         email_pw: 'BailyRobertson'
