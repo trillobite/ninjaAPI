e54f49f8d7611f39576557a67fe3bdbc9bfbac6e
patterncoder <patterncoder@gmail.com>
2016-01-18 09:58:09 -0800

    finsihed merging schema tree mods

--- server/domainModels/index.js
+++ server/domainModels/index.js
@@ -12,22 +12,6 @@ mongoose.model('Menu', schemas.production.Menu);
 mongoose.model('MenuGroup', schemas.production.MenuGroup);
 mongoose.model('Customer', schemas.customer.Customer);
 
-<<<<<<< HEAD
-// import schemas
-//var companySchema = require('./schemas/account/Company');
-//var userSchema = require('./schemas/account/User');
-// register the models with the imported schemas
-mongoose.model('User', require('./serverSchemas/User'));
-mongoose.model('Company', require('./schemas/account/Company'));
-mongoose.model('Lookups', require('./schemas/common/Lookups'));
-// mongoose.model('User', userSchema.schema);
-// mongoose.model('Company', companySchema.schema);
-mongoose.model('Menu', require('./schemas/production/Menu'));
-mongoose.model('Venue', require('./schemas/events/venueSchema'));
-mongoose.model('RentalItem', require('./schemas/events/rentalItemSchema'));
-mongoose.model('MenuItem', require('./schemas/production/MenuItem'));
-mongoose.model('MenuGroup', require('./schemas/production/MenuGroup'));
-=======
->>>>>>> 5bc7621fce8fd476e3116e1549c52f912c9ad3c1
+
 
 exports.seedDb = require('./seedDb');
