62279f805654f98a65e00456992b2d727478c902
Michael <michaelrobertson74@hotmail.com>
2016-01-27 08:38:14 -0800

    added menu groups and menus

--- server/routes/v1/production/menuGroups/menuGroupsRouter.js
+++ server/routes/v1/production/menuGroups/menuGroupsRouter.js
@@ -1,15 +1,29 @@
 var express = require('express');
-var controller = require('./menuGroupsCtrl');
+var controller = require('../../crudCtrl');
+var MenuGroup = require('mongoose').model('MenuGroup');
 var tokenProtection = require('../../../../config/routeMiddleware');
 
+
+
+ //http://stackoverflow.com/questions/15995461/node-js-pass-variable-to-route
 var router = express.Router();
 
 router.use(tokenProtection);
-router.get('/', controller.getMenuGroups);
-// router.get('/:id', controller.getMenuGroupById);
-// router.delete('/:id', controller.deleteMenuGroup);
-// router.put('/:id', controller.updateMenuGroup);
-// router.post('/', controller.createMenuGroup);
+router.get('/', function(req, res){
+  controller.getModelItems(req, res, MenuGroup);
+});
+router.get('/:id', function(req, res){
+  controller.getModelItemById(req, res, MenuGroup);
+});
+router.delete('/:id', function(req, res){
+  controller.deleteModelItem(req, res, MenuGroup);
+});
+router.put('/:id', function(req, res){
+  controller.updateModelItem(req, res, MenuGroup);
+});
+router.post('/', function(req, res){
+  controller.createModelItem(req, res, MenuGroup);
+});
 
 
 module.exports = router;
--- server/routes/v1/production/menus/menusRouter.js
+++ server/routes/v1/production/menus/menusRouter.js
@@ -1,14 +1,29 @@
 var express = require('express');
-var controller = require('./menusCtrl');
+var controller = require('../../crudCtrl');
+var Menu = require('mongoose').model('Menu');
 var tokenProtection = require('../../../../config/routeMiddleware');
 
+
+
+ //http://stackoverflow.com/questions/15995461/node-js-pass-variable-to-route
 var router = express.Router();
+
 router.use(tokenProtection);
-router.get('/', controller.getMenus);
-// router.get('/:id', controller.getMenuById);
-// router.delete('/:id', controller.deleteMenu);
-// router.put('/:id', controller.updateMenu);
-// router.post('/', controller.createMenu);
+router.get('/', function(req, res){
+  controller.getModelItems(req, res, Menu);
+});
+router.get('/:id', function(req, res){
+  controller.getModelItemById(req, res, Menu);
+});
+router.delete('/:id', function(req, res){
+  controller.deleteModelItem(req, res, Menu);
+});
+router.put('/:id', function(req, res){
+  controller.updateModelItem(req, res, Menu);
+});
+router.post('/', function(req, res){
+  controller.createModelItem(req, res, Menu );
+});
 
 
 module.exports = router;
