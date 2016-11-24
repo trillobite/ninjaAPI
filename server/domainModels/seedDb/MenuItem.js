af9566cb1a6ce94e2e7b109818610f0a01c601ca
patterncoder <chrisbaily@Chriss-MBP.home>
2016-11-24 08:36:03 -0800

    started on seed data

--- server/domainModels/seedDb/MenuItem.js
+++ server/domainModels/seedDb/MenuItem.js
@@ -5,35 +5,77 @@ module.exports = createDefaultMenuItems;
 
 function createDefaultMenuItems(companyId) {
     var dfd = Q.defer();
-    
+
     var items = [];
     MenuItem.find({}).exec(function (err, collection) {
-         if (err) {
+        if (err) {
             console.log(err.toString());
         }
         if (collection.length === 0) {
-            var menuItem1 = {meta:{company: companyId}, name: 'Caesar Salad', description: 'basic Ceasar', title: 'Ceaser Salad', subtitle: 'Crisp romaine lettuce tossed with Caesar dressing, croutons and asiago cheese', notes: 'some ceaser notes', category: 'Salad', linkeditems: []};
-            var menuItem2 = {meta:{company: companyId}, name: 'House Salad', description: 'basic house salad', title: 'House Salad', subtitle: 'Mixed greens tossed with raspberry vinaigrette and toasted almonds', notes: 'some house notes', category: 'Salad', linkeditems: []};
-            
-            MenuItem.create(menuItem1, menuItem2, function (err, item1, item2) {
+            var menuItem1 = {
+                meta: { company: companyId },
+                name: 'Mexican Fiesta',
+                description: `Southwest Caesar Salad, Pork Tenderloin with Tomatillo Sauce, Chicken Enchiladas, Spanish Rice, Tortilla Chips, Salsa, Homemade Guacamole, Flan Cups`,
+                title: 'Mexican Fiesta',
+                subtitle: 'Mexican Fiesta',
+                notes: '',
+                category: 'Buffet',
+                linkeditems: []
+            };
+            var menuItem2 = { 
+                meta: { company: companyId }, 
+                name: 'Italian Buffet', 
+                description: `Panzanella Salad, Grilled Vegetable Lasagna, Penne Bolognese, Grilled Squash and Eggplant, Garlic Cheese Toast, Tiramisu Cups`, 
+                title: 'Italian Buffet', 
+                subtitle: 'Italian Buffet', 
+                notes: '', 
+                category: 'Buffet', 
+                linkeditems: [] };
+            var menuItem3 = { 
+                meta: { company: companyId }, 
+                name: 'Taste of Thai', 
+                description: `Green Salad with Chili Lime Dressing, Thai Chicken Pasta, Thai Beef, Red Curry Rissotto, Spring Rolls, Thai Creme Caramel`, 
+                title: 'Taste of Thai', 
+                subtitle: 'Taste of Thai', 
+                notes: '', 
+                category: 'Buffet', 
+                linkeditems: [] };
+            var menuItem4 = { 
+                meta: { company: companyId }, 
+                name: 'French Elegance', 
+                description: `Mixed Greens with French Dressing, Beef Bourguignon, Chicken Cordon Bleu, Potatoes au Gratin, Ratatouille, Strawberry Crepes`, 
+                title: 'French Elegance', 
+                subtitle: 'French Elegance', 
+                notes: '', 
+                category: 'Buffet', 
+                linkeditems: [] };
+
+            MenuItem.create(menuItem1, 
+                menuItem2, 
+                menuItem3, 
+                menuItem4, function (err, 
+                item1, 
+                item2, 
+                item3, 
+                item4) {
                 if (err) {
                     console.log(err.toString());
                 }
                 items.push(item1);
                 items.push(item2);
                 console.log('30 succesfully created menuitem documents.....');
-                
+
                 dfd.resolve(items);
-                
+
             });
-            
-            
-           
+
+
+
         }
-        
+
     });
-    
+
     return dfd.promise;
-    
+
 }
 
