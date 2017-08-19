67272c4e7b71c4311de70f64152726f592b8da68
Michael Robertson <mrobertson@nationalmerchants.org>
2017-08-19 11:25:27 -0700

    Fix the contract add fuzzy search functionality

--- server/routes/v1/customers/searchController.js
+++ server/routes/v1/customers/searchController.js
@@ -1,20 +1,45 @@
 exports.searchCustomers = function (req, res, model) {
 
+    console.log("request");
     console.log(req.query);
+   
 
+    var searchName = {};
+    // if two names, set them as first and last, if only one name,
+    // search both first and last names by the give name
+    if (req.query.lastName) {
+        searchName = {
+            $and: [
+                {'firstName': new RegExp(req.query.firstName, 'i')},
+                {'lastName': new RegExp(req.query.lastName, 'i')}
+            ]
+        };
+    } else {
+        searchName = {
+            $or: [
+                {'firstName': new RegExp(req.query.firstName, 'i')},
+                {'lastName': new RegExp(req.query.firstName, 'i')}
+            ]
+        };
+    }
+
+    console.log("search params");
+    console.log(searchName);
     model.find({ 
         $and: [
-            {$or:[{'firstName': new RegExp(req.query.name, 'i')}, {'lastName': new RegExp(req.query.name, 'i')}]},
-            {'meta.company':req.user.company}
-        ]}).select('firstName lastName fullName').exec(function(err, collection){
+            searchName,
+            // {$or:[{'firstName': new RegExp(req.query.firstName, 'i')}, {'lastName': new RegExp(req.query.lastName, 'i')}]},
+            {'meta.company':req.user.company} ]}).select('firstName lastName fullName').exec(function(err, collection){
         if(err){
             res.status(500);
-            return res.send({reason: err.toString()})
+            return res.send({reason: err.toString()});
         }
         if (!collection.length) {
             //res.status(404);
-            return res.send({noData: true, data: collection})
+            return res.send({noData: true, data: collection});
         }
+        console.log("collection");
+        console.log(collection);
         res.send({data:collection});
-    })
-}
+    });
+};
