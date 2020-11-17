abe120f2e99f3b0b5d8671cd4d2d02b6ddea08ec
trillobite <jparnell0@protonmail.com>
2020-11-16 22:30:47 -0800

    now shows rental items

--- server/routes/v1/events/contracts/contractTemplate.js
+++ server/routes/v1/events/contracts/contractTemplate.js
@@ -3,56 +3,83 @@ var dateUtils = require('../../../../utilities/dates');
 var numUtils = require('../../../../utilities/numbers');
 var strUtils = require('../../../../utilities/strings');
 
-module.exports = function(data) {
+module.exports = function (data) {
 
-  
+  // console.log("pdf data:", escape(data.notes));
+
   const sectionEventSteps = (events) => {
     return events && events.length > 0 ?
       `"Event Steps"`
       : "{}"
   };
   const sectionRentalItems = (rentalItems) => {
-    return rentalItems && rentalItems.length > 0 ?
-      `"Rental Items"`
-      : "{}"
+
+    let tmp = (arr) => {
+      console.log("rentalItems:", rentalItems);
+      return arr.map((ri) => {
+        console.log("rentalItem:", ri);
+        return `[
+              { "stack": [ { "text": "${ri.name}", "style": ["bold", "fontSize10"] }] },
+              {"text": "${ri.quantity}", "style": ["fontSize8"]},
+              {"text": "${numUtils.convertToCurrencyString(ri.price)}", "style": ["fontSize8"]},
+              {"text": "${numUtils.convertToCurrencyString(ri.quantity * ri.price)}", "style": ["fontSize8"]}
+            ]`;
+      }).join(",");
+    }
+
+    let rentalTable = `{
+        "style": "tableExample",
+        "table": {
+          "widths": ["*", 72, 72, 72],
+          "body": [
+            ["Rental Item", "Quantity", "Price", "Extended"],
+            ${tmp(rentalItems)}
+          ]
+        }
+      }`
+
+    return rentalTable;
   };
 
   let jsonDef = `
   {
 
-  "pageSize": "letter",
-  "pageOrientation":  "landscape",
+  "pageSize": "LETTER",
+  "pageOrientation":  "portrait",
   "pageMargins": [ 18, 18, 18, 18 ],
   "content": [
+      {"image": "./bailys_logo.JPG", "alignment": "justify"},
       {
         "alignment": "justify",
         "columns": [
           {
             "stack": [
-              { "text": "Customer", "style": ["fontSize16", "bold"] },
-              { "text": ${`"${data.customer.firstName} ${data.customer.lastName}"`}, "style": ["fontSize12"] }
-              ${(data.customer.phoneNumbers.length > 0) ? 
-                  `,${data.customer.phoneNumbers.map(pn => `{ "text": "${pn.number} (${pn.contactType})", "style": [] }`).join(",")}`
-                  : ""}
+              { "text": "Customer", "style": ["fontSize12", "bold"] },
+              { "text": ${`"${data.customer.firstName} ${data.customer.lastName}"`}, "style": ["fontSize10"] }
+              ${(data.customer.phoneNumbers.length > 0) ?
+      `,${data.customer.phoneNumbers.map(pn => `{ "text": "${pn.number} (${pn.contactType})", "style": [] }`).join(",")}`
+      : ""}
               ${(data.customer.emails.length > 0) ?
-                  `,${data.customer.emails.map(em => `{ "text": "${em.email} (${em.emailType})", "style": [] }`).join(",")}`
-                  : ""}
+      `,${data.customer.emails.map(em => `{ "text": "${em.email} (${em.emailType})", "style": [] }`).join(",")}`
+      : ""}
                   
             ]
           },
           {
             "stack": [
-              { "text": "Event Details", "style": ["fontSize16", "bold"] },
+              { "text": "Event Details", "style": ["fontSize12", "bold"] },
               { "text": ${`"Event Name: ${data.eventName}"`}, "style": [] },
               { "text": ${`"Event Date: ${
-                dateUtils.dateFormat1(data.eventDate)
-              }"`}, "style": [] },
+    dateUtils.dateFormat1(data.eventDate)
+    }"`}, "style": [] },
               { "text": ${`"Nature of Event: ${data.natureOfEvent}"`}, "style": [] }
             ]
           }
         ]
       },
-      {"text": "Menu Items", "style": ["fontSize16", "marginTopBottom18"]},
+      {"text": "Notes", "style": ["fontSize12", "marginTopBottom18"]},
+      {"text": "${data.notes.replace(/\n/g, "\\n")}", "style": ["fontSize8"]},
+      {"text": "Menu Items", "style": ["fontSize12", "marginTopBottom18"]},
       {
         "style": "tableExample",
         "table": {
@@ -60,13 +87,16 @@ module.exports = function(data) {
           "body": [
             ["Menu Item", "Quantity", "Price", "Extended"],
             ${data.menuItems.map(mi => {
-              return `[
-                { "stack": [ { "text": "${mi.name}", "style": ["bold"] }, { "text": "${strUtils.stripNewLines(mi.description)}" } ] },
-                {"text": "${mi.quantity}", "style": []},
-                {"text": "${numUtils.convertToCurrencyString(mi.price)}", "style": []},
-                {"text": "${numUtils.convertToCurrencyString(mi.quantity * mi.price)}", "style": []}
-              ]`
-            }).join(",")}
+            return `[
+                      { "stack": [ 
+                        { "text": "${mi.name}", "style": ["bold", "fontSize10"]}, 
+                        { "text": "${strUtils.stripNewLines(mi.description)}", "style": ["fontSize8"]} 
+                      ]},
+                      {"text": "${mi.quantity}", "style": ["fontSize8"]},
+                      {"text": "${numUtils.convertToCurrencyString(mi.price)}", "style": ["fontSize8"]},
+                      {"text": "${numUtils.convertToCurrencyString(mi.quantity * mi.price)}", "style": ["fontSize8"]}
+                    ]`
+             }).join(",")}
           ]
         }
       },
@@ -76,32 +106,47 @@ module.exports = function(data) {
   }
   `
 
+  // {"text": "Notes", "style": ["fontSize16", "marginTopBottom18"]},
+  // {
+  //   "style": "tableExample",
+  //   "table": {
+  //     "widths": ["*"],
+  //     "body": [
+  //       ["Notes"],
+  //       [{"text": "${escape(data.notes)}", "style": []}],
+  //     ]
+  //   }
+  // },
+  console.log("docDef:", jsonDef);
+
   let docDef = JSON.parse(jsonDef);
+
+
   docDef.styles = {
-		header: {
-			fontSize: 18,
-			bold: true,
-			alignment: "right",
-			margin: [0, 190, 0, 80]
-		},
-		subheader: {
-			fontSize: 14
-		},
-		superMargin: {
-			margin: [20, 0, 40, 0],
-			fontSize: 15
+    header: {
+      fontSize: 18,
+      bold: true,
+      alignment: "right",
+      margin: [0, 190, 0, 80]
+    },
+    subheader: {
+      fontSize: 12
+    },
+    superMargin: {
+      margin: [20, 0, 40, 0],
+      fontSize: 13
     },
     marginTopBottom18: {
       margin: [0, 18]
     },
-		tableExample: {
-			margin: [0, 5, 0, 15]
-		},
-		tableHeader: {
-			bold: true,
-			fontSize: 13,
-			color: "black"
-		},
+    tableExample: {
+      margin: [0, 5, 0, 15]
+    },
+    tableHeader: {
+      bold: true,
+      fontSize: 11,
+      color: "black"
+    },
     bold: {
       bold: true
     },
@@ -123,7 +168,7 @@ module.exports = function(data) {
     fontSize16: {
       fontSize: 16
     }
-	}
+  }
 
   return docDef;
 }
